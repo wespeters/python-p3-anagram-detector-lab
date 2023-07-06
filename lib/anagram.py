c1d7317befdc8923e5e67e49714adbbdaa4fa9bc
Wesley Peters <wesleypeters@gmail.com>
2023-07-05 19:55:41 -0600

    define class and methods

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word.lower())
+
+    def match(self, possible_matches):
+        return [w for w in possible_matches if sorted(w.lower())
+               == self.sorted_word and w.lower() != self.word.lower()]
